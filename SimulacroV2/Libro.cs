dc143fe10ed5683a3fab1f095fb7f5d85eb0afbe
Luis Perez <argenisperezcabrera@gmail.com>
2026-05-07 12:44:28 +0200

    Ejercicio2

---
+++ SimulacroV2/Libro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacroV2
+{
+    internal class Libro
+    {
+        private string titulo { get; set; }
+        private string autor { get; set; }
+        private int anyoPublicacion { get; set; }
+        private bool disponible { get; set; } = true;
+        public Libro(string titulo, string autor, int anyoPublicacion, bool disponible)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.anyoPublicacion = anyoPublicacion;
+            this.disponible = disponible;
+        }
+        public override string ToString()
+        {
+            return $"{titulo} - {autor} ({anyoPublicacion})";
+        }
+    }
+}
--- SimulacroV2/Program.cs
+++ SimulacroV2/Program.cs
@@ -1,19 +1,51 @@
-﻿public class Libro
+﻿using SimulacroV2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SimulacroV2
 {
-    public string titulo { get; set; }
-    public string autor { get; set; }
-    public int anyoPublicacion { get; set; }
-    public bool disponible { get; set; } = true;
-    public Libro(string titulo, string autor, int anyoPublicacion,bool disponible)
+    internal class Program
     {
-        titulo = titulo;
-        autor = autor;
-        anyoPublicacion = anyoPublicacion;
-        disponible = disponible;
-    }
-    public override string ToString()
-    {
-        return $"{titulo} - {autor} ({anyoPublicacion})";
-    }
+        // Cambia el método guardarLibro a estático para que pueda ser llamado desde Main
+        public static void guardarLibro(List<Libro> libros, string ruta)
+        {
+            StreamWriter escritor = File.CreateText(ruta);
+            foreach (Libro libro in libros)
+            {
+                escritor.WriteLine(libro.ToString());
+            }
+            escritor.Close();
+        }
 
+        public static void Main(String[] args)
+        {
+
+            List<Libro> libros = new List<Libro>();
+            libros.Add(new Libro("El Quijote", "Miguel de Cervantes", 1605, true));
+            libros.Add(new Libro("Cien años de soledad", "Gabriel García Márquez", 1967, true));
+            libros.Add(new Libro("La sombra del viento", "Carlos Ruiz Zafón", 2001, true));
+
+            foreach (Libro libro in libros)
+            {
+                Console.WriteLine(libro.ToString());
+
+            }
+            foreach (Libro libro in libros)
+            {
+                if (libro.ToString().Contains("Miguel"))
+                {
+                    Console.WriteLine("El Libro que contiene a este autor es: " + libro.ToString());
+                }
+            }
+
+            DateTime fechaActual = DateTime.Now;
+            Console.WriteLine(fechaActual.ToShortDateString());
+            foreach (Libro libro in libros)
+            {
+                guardarLibro(libros, "D:\\EDE\\libros.txt");
+            }
+        }
+    }
 }
