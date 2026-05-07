2d0dc37af3ddcad62d22ec9989ce19f7651d152d
Luis Perez <argenisperezcabrera@gmail.com>
2026-05-07 12:10:00 +0200

    Ejercicio1

--- SimulacroV2/Program.cs
+++ SimulacroV2/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿public class Libro
+{
+    public string titulo { get; set; }
+    public string autor { get; set; }
+    public int anyoPublicacion { get; set; }
+    public bool disponible { get; set; } = true;
+    public Libro(string titulo, string autor, int anyoPublicacion,bool disponible)
+    {
+        titulo = titulo;
+        autor = autor;
+        anyoPublicacion = anyoPublicacion;
+        disponible = disponible;
+    }
+    public override string ToString()
+    {
+        return $"{titulo} - {autor} ({anyoPublicacion})";
+    }
+
+}
